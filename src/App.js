539c8dbe729ff1ad029bfac4f0c799b9216e929e
Nisanad <s6135512035@phuket.psu.ac.th>
2020-09-17 00:26:26 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
+const word = "Hello";
+function App() {
+ return (
+  <div>
+  <WordCard value="hello"/>
+  {/*  */}
+ 
+  </div>
+ );
+ 
+}
+
+export default App;
